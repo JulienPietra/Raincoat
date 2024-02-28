e33b0412ec9e150f3a3e56d6a95b3a124023d9d3
JulienPietra <144248552+JulienPietra@users.noreply.github.com>
2024-02-28 23:18:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ args = parser.parse_args()
 if __name__ == "__main__":
     trainer = cross_domain_trainer(args)
     trainer.train()
-    # trainer.visualize()
-    # dic = {'1':trainer.src_all_features,'2':trainer.src_true_labels,'3':trainer.trg_all_features,'4':trainer.trg_true_labels,'acc': trainer.trg_acc_list}
-    # with open('saved_dictionary2.pickle', 'wb') as handle:
-    #     pickle.dump(dic, handle)
+    trainer.visualize()
+    dic = {'1':trainer.src_all_features,'2':trainer.src_true_labels,'3':trainer.trg_all_features,'4':trainer.trg_true_labels,'acc': trainer.trg_acc_list}
+    with open('saved_dictionary2.pickle', 'wb') as handle:
+        pickle.dump(dic, handle)
     
